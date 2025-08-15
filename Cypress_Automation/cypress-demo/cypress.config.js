4438047274ca2156135103313b319febb15afbe6
Arunkambrekar <arunkabrekar@gmail.com>
2025-08-15 16:53:58 +0530

    updated

--- Cypress_Automation/cypress-demo/cypress.config.js
+++ Cypress_Automation/cypress-demo/cypress.config.js
@@ -2,9 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3000", // change to your dev server's port
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "http://localhost:3000",
+    defaultCommandTimeout: 30000, // 30 seconds
   },
 });
+
